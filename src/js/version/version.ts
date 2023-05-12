08afcfbef7fc94092fafef71b32fe7fb343ba0a4
Johnny Noble <github-a3bd@johnny.net.au>
2023-05-12 12:50:09 +1000

    merge release/5.0.15

--- src/js/version/version.ts
+++ src/js/version/version.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const version = {
-  number: "5.0.14",
-=======
-export const version = {
   number: "5.0.15",
->>>>>>> release/5.0.15
   date: new Date().toISOString().split("T")[0],
 };
 
