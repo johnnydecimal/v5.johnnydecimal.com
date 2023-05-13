9cf073e0b012444bd36d43b16ccad223f55cd1a8
Johnny Noble <github-a3bd@johnny.net.au>
2023-05-13 17:16:22 +1000

    cypress tests

--- cypress/e2e/11.01.cy.js
+++
@@ -1,6 +0,0 @@
-it("titles are correct", () => {
-  const page = cy.visit("/");
-
-  page.get("title").should("have.text", "J•D");
-  page.get("h1").should("have.text", "A system to organise projects");
-});
