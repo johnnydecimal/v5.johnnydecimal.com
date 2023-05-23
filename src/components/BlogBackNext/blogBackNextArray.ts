3b3dbd120b605a331d5a174583813d6227031d5d
Johnny Noble <github-a3bd@johnny.net.au>
2023-05-23 10:25:44 +1000

    blog layout: progress

---
+++ src/components/BlogBackNext/blogBackNextArray.ts
@@ -0,0 +1,36 @@
+import { getCollection } from "astro:content";
+
+const sitePages = await getCollection("blog", ({ data }) =>
+  import.meta.env.DEV ? true : data.publish
+);
+
+interface BackNext {
+  pathname?: string;
+  title?: string;
+}
+const backNextArray: BackNext[] = [];
+
+sitePages.forEach((page) => {
+  // Are we in the same area as the previous entry?
+  // If *not*, drop in an `undefined`.
+
+  // Type safety: does backNextArray[-1].pathname exist?
+  if (backNextArray.length > 0) {
+    if (
+      backNextArray[backNextArray.length - 1].pathname?.substring(1, 6) !==
+      page.slug.substring(0, 5) // no slashes on this one
+    ) {
+      backNextArray.push({ pathname: undefined });
+    }
+  }
+
+  // We only push IDs to backNextArray: no areas, no categories.
+  if (page.slug.split("/").length === 3) {
+    backNextArray.push({
+      pathname: "/" + page.slug.replace(/(\d\d)(\d\d)/, "$1.$2") + "/",
+      title: page.data.title,
+    });
+  }
+});
+
+export default backNextArray;
